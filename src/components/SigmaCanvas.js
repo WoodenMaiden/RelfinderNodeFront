e5ad6a32a665afcf5b3af51097d7959bc8c2e59a
Yann P <yann.pomie@laposte.net>
2022-03-24 16:54:29 +0100

    feat: screenshot

--- src/components/SigmaCanvas.js
+++ src/components/SigmaCanvas.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {MultiDirectedGraph} from "graphology";
 import Sigma from "sigma";
+import FileSaver from "file-saver"
 
 import './SigmaCanvas.css'
 
@@ -123,47 +124,101 @@ export default function SigmaCanvas(props) {
         }
     }
 
+    function screenshot(event) {
+        //https://codesandbox.io/s/github/jacomyal/sigma.js/tree/main/examples/png-snapshot?file=/saveAsPNG.ts:27-63
+        const {width, height} = sigma.getDimensions()
+        const pixelRatio = window.devicePixelRatio || 1;
+
+        const hidden = document.getElementById("hidden")
+        hidden.style.width = `${width}px`;
+        hidden.style.height = `${height}px`;
+        hidden.style.position = "absolute";
+        hidden.style.right = "101%";
+        hidden.style.bottom = "101%";
+
+        const tmpRenderer = new Sigma(sigma.getGraph(), hidden, sigma.getSettings());
+
+        tmpRenderer.getCamera().setState(sigma.getCamera().getState());
+        tmpRenderer.refresh();
+
+        const canvas = document.createElement("CANVAS")
+        canvas.setAttribute("width", width * pixelRatio + "")
+        canvas.setAttribute("height", height * pixelRatio + "")
+        const ctx = canvas.getContext("2d")
+
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, width * pixelRatio, height * pixelRatio)
+
+        const canvases = tmpRenderer.getCanvases()
+        const layers = Object.keys(canvases)
+        layers.forEach(id => {
+            ctx.drawImage(
+                canvases[id],
+                0,
+                0,
+                width * pixelRatio,
+                height * pixelRatio,
+                0,
+                0,
+                width * pixelRatio,
+                height * pixelRatio,
+            )
+        })
+
+        canvas.toBlob(blob => {
+            const d = new Date()
+            if (blob) FileSaver.saveAs(blob, `RFR_${d.getDate()}/${d.getMonth()}/${d.getFullYear()}-${d.getHours()}:${d.getMinutes()}`)
+
+            tmpRenderer.kill();
+        }, "image/png")
+    }
+
     return (
-        <div id="sigmaroot">
-            <ul>
-                <li>
-                    <div id="search" className="deployedbutton">
-                       <form>
-                           <input type="search" id="searchinput" name="toSearch" placeholder="Node to search" onKeyPress={search}/>
-                           <span className="material-icons-round clickable" onClick={search}>
-                               search
-                           </span>
-                           <span className="material-icons-round clickable" onClick={resetGraph}>
-                              restart_alt
-                           </span>
-                       </form>
-                   </div>
-                </li>
+        <div id="sigmacanvas">
+	        <div id="sigmaroot">
+	            <ul>
+	                <li>
+	                    <div id="search" className="deployedbutton">
+	                       <form>
+	                           <input type="search" id="searchinput" name="toSearch" placeholder="Node to search" onKeyPress={search}/>
+	                           <span className="material-icons-round clickable" onClick={search}>
+	                               search
+	                           </span>
+	                           <span className="material-icons-round clickable" onClick={resetGraph}>
+	                              restart_alt
+	                           </span>
+	                       </form>
+	                   </div>
+	                </li>
 
-                <li>
-                    <div className="roundbutton clickable">
-                       <span className="material-icons-round">
-                          photo_camera
-                       </span>
-                    </div>
-                </li>
+	                <li>
+	                    <div className="roundbutton clickable" onClick={screenshot}>
+	                       <span className="material-icons-round">
+	                          photo_camera
+	                       </span>
+	                    </div>
+	                </li>
 
-                <li>
-                    <div className="roundbutton clickable">
-                       <span className="material-icons-round">
-                           add
-                       </span>
-                    </div>
-                </li>
+	                <li>
+	                    <div className="roundbutton clickable">
+	                       <span className="material-icons-round">
+	                           add
+	                       </span>
+	                    </div>
+	                </li>
 
-                <li>
-                    <div className="roundbutton clickable">
-                       <span className="material-icons-round">
-                           remove
-                       </span>
-                    </div>
-                </li>
-            </ul>
+	                <li>
+	                    <div className="roundbutton clickable">
+	                       <span className="material-icons-round">
+	                           remove
+	                       </span>
+	                    </div>
+	                </li>
+	            </ul>
+	        </div>
+	        <div id="hidden">
+
+	        </div>
         </div>
     )
 }
